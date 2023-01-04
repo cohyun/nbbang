b0c6b9d42b3296df962e417886445840638c56de
cohyun <syrup91@ewhain.net>
2023-01-05 06:01:58 +0900

    카드 추가하는 방식으로 변경 2

--- src/App.js
+++ src/App.js
@@ -4,27 +4,11 @@ import { users } from "./Userlist";
 import "./App.css";
 let idNum = 1;
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newEmail, setNewEmail] = useState("");
-  const [refresh, setFresh] = useState(0);
-
   return (
     <div class="Back">
-      <input placeholder="이름" onChange={(e) => setNewName(e.target.value)} />
-      <input
-        placeholder="이메일"
-        onChange={(e) => setNewEmail(e.target.value)}
-      />
       <button
         onClick={() => {
-          users.push({
-            id: idNum,
-            username: newName,
-            email: newEmail,
-          });
-          idNum++;
           console.log(users);
-          setFresh(refresh + 1);
         }}
       >
         안녕
--- src/Userlist.js
+++ src/Userlist.js
@@ -1,9 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
 function User({ user }) {
+  const [newName, setnewName] = useState("");
+  const [newMoney, setnewMoney] = useState(0);
   return (
     <div className="UserCard">
-      <input className="NameInput" placeholder="이름" />
+      <input
+        className="NameInput"
+        placeholder="이름"
+        onChange={(e) => {
+          user.username = e.target.value;
+        }}
+      />
       <input className="MoneyInput" placeholder="금액 (숫자만 입력)" />
       {/* <b>{user.username}</b> <a>({user.money}원)</a> */}
     </div>
@@ -13,22 +22,38 @@ const users = [
   {
     id: 1,
     username: "이름",
-    money: null,
+    money: 0,
   },
   {
     id: 2,
     username: "이름",
-    money: null,
+    money: 0,
   },
 ];
+let idNum = 3;
 
 function UserList() {
+  const [refresh, setFresh] = useState(0);
+
   return (
     <div className="CardBack">
       {users.map((user) => (
         <User user={user} key={user.id} />
       ))}
-      <div className="UserCard">+</div>
+      <div
+        className="UserCard"
+        onClick={() => {
+          users.push({
+            id: idNum,
+            username: "이름",
+            money: 0,
+          });
+          idNum++;
+          setFresh(refresh + 1);
+        }}
+      >
+        +
+      </div>
     </div>
   );
 }
