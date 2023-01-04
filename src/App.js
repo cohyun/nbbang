7f873e91c10210d3c9473c40e36c4aeec2f01dfc
cohyun <syrup91@ewhain.net>
2023-01-05 02:56:14 +0900

    사용자 추가 / 배열 렌더링 / css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import UserList from "./Userlist";
-
+import { users } from "./Userlist";
+import "./App.css";
+let idNum = 1;
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [refresh, setFresh] = useState(0);
+
   return (
-    <div>
-      <button>안녕</button>
+    <div class="Back">
+      <input placeholder="이름" onChange={(e) => setNewName(e.target.value)} />
+      <input
+        placeholder="이메일"
+        onChange={(e) => setNewEmail(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          users.push({
+            id: idNum,
+            username: newName,
+            email: newEmail,
+          });
+          idNum++;
+          console.log(users);
+          setFresh(refresh + 1);
+        }}
+      >
+        안녕
+      </button>
+
       <UserList />
-      <div> {UserList}</div>
     </div>
   );
 }
--- src/Userlist.js
+++ src/Userlist.js
@@ -1,37 +1,24 @@
+import { useEffect } from "react";
+import "./App.css";
 function User({ user }) {
   return (
-    <div>
+    <div className="UserCard">
       <b>{user.username}</b> <span>({user.email})</span>
     </div>
   );
 }
+const users = [];
 
 function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
-
   return (
-    <div>
+    <div className="CardBack">
       {users.map((user) => (
         <User user={user} key={user.id} />
       ))}
     </div>
   );
 }
+
+export { users, UserList };
 
 export default UserList;
